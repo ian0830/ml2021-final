8875776bc9e5d25d93067ac6ff9d74b3eb84e6ce
Codeamon <LC13E7.CF0cl@gmail.com>
2022-01-15 15:31:31 +0800

    modify execute with little changes Append current version of result, feature of value-category

--- execute.py
+++ execute.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+from unittest import TestProgram
 from csvfiledata import *
 files = [
     'demographics.csv', 'location.csv', 'satisfaction.csv', 'services.csv', 'status.csv']
+
+def test_print(data, lenLimit = 1):
+    if len(data) < lenLimit: print(*data, sep='\n')
+    else: 
+        for i in range(lenLimit): print(data[i])
 
 if __name__ == '__main__':
     # merging to a big spreadsheet
@@ -13,7 +19,7 @@ if __name__ == '__main__':
         else: 
             resultHeader, resultContent = merge_file(resultContent, content, resultHeader, header)
     
-    # 只處理我們選定的id 的資料
+    # Extract sample_submission IDs and sorted from the big spreadsheet
     # sample_submission: Test_IDs
     # status: Train_IDs
     header, content = get_file('sample_submission.csv')
@@ -23,10 +29,12 @@ if __name__ == '__main__':
         sortedContent.append([row[0:] for row in resultContent if row[0] == _id][0])
     resultContent = sortedContent
 
-    # move Churn Category to first column
+    # move Churn Category to second column
+    # category list: ID, Churn category, ...
     statusIndex = resultHeader.index('Churn Category')
-    resultHeader = [resultHeader[0]] + [resultHeader[statusIndex]] + resultHeader[1:statusIndex]+resultHeader[statusIndex+1:] 
-    for i in range(len(resultContent)): resultContent[i] = [resultContent[i][0]] + [resultContent[i][statusIndex]] + resultContent[i][1:statusIndex]+resultContent[i][statusIndex+1:]
+    resultHeader = [resultHeader[0]] + [resultHeader[statusIndex]] + resultHeader[1:statusIndex] + resultHeader[statusIndex+1:]
+    for i in range(len(resultContent)):
+        resultContent[i] = [resultContent[i][0]] + [resultContent[i][statusIndex]] + resultContent[i][1:statusIndex] + resultContent[i][statusIndex+1:]
 
     # append population by zip code to each row
     popHeader, popContent = get_file('population.csv')
@@ -41,11 +49,12 @@ if __name__ == '__main__':
             appendContent.append([transformation[float(row[index])]])
         else:
             appendContent.append([''])
+    
     resultHeader = resultHeader[0: index] + [appendHeader] + resultHeader[index:]
     for i in range(len(resultContent)): resultContent[i] = resultContent[i][0: index] + appendContent[i]+ resultContent[i][index:]
 
-    # exclude irrelevant value
-    exclude = ['Count', 'Country', 'State', 'City', 'Zip Code','Lat Long',  'Quarter']
+    # exclude irrelevant value (meaningless)
+    exclude = ['Count', 'Country', 'State', 'City', 'Zip Code','Lat Long', 'Quarter']
     for target in exclude:
         index = resultHeader.index(target)
         resultHeader = resultHeader[0: index] + resultHeader[index+1:]
@@ -58,19 +67,17 @@ if __name__ == '__main__':
         index = resultHeader.index(feature)
         values = set()
         for row in resultContent:
-            if row[index] != '' and row[index]!=-1: values.add(row[index])
+            if row[index] != '': values.add(row[index])
         values = list(values)
-        appendHeader = [feature + '-Empty'] + [feature+ '-' + value for value in values]
+        appendHeader = [feature + '-' + value for value in values]
         appendContent = []
         for row in resultContent:
-            appendRow = [0 for _ in appendHeader]
-            if (row[index] == -1 or row[index] == ''):
-                appendRow[0] = 1
-            else:
+            appendRow = [0]*len(appendHeader)
+            if not (row[index] == ''):
                 appendRow[appendHeader.index(feature + '-' + row[index])] = 1
             appendContent.append(appendRow)
-        resultHeader = resultHeader[0: index] + appendHeader[1:] + resultHeader[index+1:]
-        for i in range(len(resultContent)): resultContent[i] = resultContent[i][0: index] + appendContent[i][1:]  + resultContent[i][index+1:]
+        resultHeader = resultHeader[0: index] + resultHeader[index+1:] + appendHeader
+        for i in range(len(resultContent)): resultContent[i] = resultContent[i][0: index] + resultContent[i][index+1:] + appendContent[i]
 
     status = {
         'No Churn': 0,
@@ -90,7 +97,7 @@ if __name__ == '__main__':
     # resultHeader = [resultHeader[i] for i in range(len(resultHeader)) if i in selectedIndex]
     # for i in range(len(resultContent)): resultContent[i] = [resultContent[i][j] for j in range(len(resultContent[i])) if j in selectedIndex]
 
-    print(resultHeader)
+    # print(resultHeader)
     
     # 輸出到result.csv
     write_file('result.csv', resultHeader[1:],[row[1:] for row in resultContent])
